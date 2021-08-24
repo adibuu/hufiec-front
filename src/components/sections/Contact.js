dc2c71566434252f42ab56f2c85b91828d27af76
Adrian <adibu8@gmail.com>
2021-08-24 19:13:22 +0200

    HUFF-3 Fix iframe

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -121,6 +121,7 @@ export default function S3c() {
       </Flex>
       <div style={{ width: "100vw" }}>
         <iframe
+          title="map"
           width="100%"
           height="600"
           frameborder="0"
